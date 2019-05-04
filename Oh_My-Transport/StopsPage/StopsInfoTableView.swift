f1239aeec13033bb12b2c0b8666f2e1c2eb7e499
DanistyWuKids(づ｡◕‿‿◕｡)づ <wgong0001@gmail.com>
2019-05-05 04:20:23 +1000

    Stops page dependency files added

---
+++ Oh_My-Transport/StopsPage/StopsInfoTableView.swift
@@ -0,0 +1,21 @@
+//
+//  StopsInfoTableView.swift
+//  Oh_My-Transport
+//
+//  Created by OriWuKids on 5/5/19.
+//  Copyright © 2019 wgon0001. All rights reserved.
+//
+
+import UIKit
+
+class StopsInfoTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Oh_My-Transport/StopsPage/nextArrive.swift
@@ -0,0 +1,21 @@
+//
+//  nextArrive.swift
+//  Oh_My-Transport
+//
+//  Created by OriWuKids on 3/5/19.
+//  Copyright © 2019 wgon0001. All rights reserved.
+//
+
+import UIKit
+
+class nextArrival: UITableView {
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
