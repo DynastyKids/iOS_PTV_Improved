488d5eabd5ae90650585282330494c6582582261
DanistyWuKids(づ｡◕‿‿◕｡)づ <wgong0001@gmail.com>
2019-05-05 20:02:56 +1000

    Adding user notification module

--- Oh_My-Transport/AppDelegate.swift
+++ Oh_My-Transport/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.sound, .alert, .badge]
+        center.requestAuthorization(options: options) { (didAllow, error) in
+            if !didAllow {
+                print("Notifications declined")
+            }
+        }
+        
         return true
     }
 
