882c58b8aca61240a1d3a54bbe45b58594d4c61c
DanistyWuKids(づ｡◕‿‿◕｡)づ <wgong0001@gmail.com>
2019-05-06 03:07:31 +1000

    Layout setting and sample coding on disruption page

--- Oh_My-Transport/Disruptions/DisruptionDetailViewController.swift
+++ Oh_My-Transport/Disruptions/DisruptionDetailViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import WebKit
+//import WebKit
 
 class DisruptionDetailViewController: UIViewController {
     
@@ -16,42 +16,43 @@ class DisruptionDetailViewController: UIViewController {
     @IBOutlet weak var disruptionStartDateLabel: UILabel!
     @IBOutlet weak var disruptionEndDateLabel: UILabel!
     @IBOutlet weak var disruptionDetailLabel: UILabel!
+
     
-    @IBOutlet weak var doneButton: UIBarButtonItem!
-    @IBOutlet weak var webkitview: WKWebView!
-    var webView: WKWebView!
-    
+
+//    var webView: WKWebView!
+//
     @IBAction func viewInWebKit(_ sender: Any) {
-        webkitview.isHidden = false
-        doneButton.isEnabled = true
+        UIApplication.shared.openURL(URL(string: "http://ptv.vic.gov.au/live-travel-updates/article/cranbourne-and-pakenham-lines-service-alterations-from-9-20am-to-7-15pm-on-sunday-12-may-2019")!)
+//        webkitview.isHidden = false
+//        doneButton.isEnabled = true
     }
+//
+//    @IBAction func dismissWebKit(_ sender: Any) {
+//        doneButton.isEnabled = false
+//        webkitview.isHidden = true
+//    }
     
-    @IBAction func dismissWebKit(_ sender: Any) {
-        doneButton.isEnabled = false
-        webkitview.isHidden = true
-    }
-    
-    override func loadView() {
-        //创建网页加载的偏好设置
-        let prefrences = WKPreferences()
-        prefrences.javaScriptEnabled = false
-        
-        //配置网页视图
-        let webConfiguration = WKWebViewConfiguration()
-        webConfiguration.preferences = prefrences
-        
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.navigationDelegate = self as? WKNavigationDelegate;
-        view = webView
-    }
+//    override func loadView() {
+//        //创建网页加载的偏好设置
+//        let prefrences = WKPreferences()
+//        prefrences.javaScriptEnabled = false
+//
+//        //配置网页视图
+//        let webConfiguration = WKWebViewConfiguration()
+//        webConfiguration.preferences = prefrences
+//
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        webView.navigationDelegate = self as? WKNavigationDelegate;
+//        view = webView
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        // Do any additional setup after loading the view.
         let myURL = URL(string: "http://ptv.vic.gov.au/live-travel-updates/article/frankston-line-buses-replacing-trains-from-8-10pm-to-last-train-on-tuesday-14-may-2019")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
+//        let myRequest = URLRequest(url: myURL!)
+//        webView.load(myRequest)
     }
 
     /*
@@ -66,28 +67,28 @@ class DisruptionDetailViewController: UIViewController {
 
 }
 
-// MARK: WKNavigationDelegate
-extension UIViewController: WKNavigationDelegate {
-    //视图开始载入的时候显示网络活动指示器
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-    }
-    
-    //载入结束后，关闭网络活动指示器
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
-    }
-    
-    //阻止链接被点击
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if navigationAction.navigationType == .linkActivated {
-            decisionHandler(.cancel)
-            
-            let alertController = UIAlertController(title: "Action not allowed", message: "Tapping on links is not allowed. Sorry!", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alertController, animated: true, completion: nil)
-            return
-        }
-        decisionHandler(.allow)
-    }
-}
+//// MARK: WKNavigationDelegate
+//extension UIViewController: WKNavigationDelegate {
+//    //视图开始载入的时候显示网络活动指示器
+//    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+//    }
+//
+//    //载入结束后，关闭网络活动指示器
+//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+//    }
+//
+//    //阻止链接被点击
+//    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+//        if navigationAction.navigationType == .linkActivated {
+//            decisionHandler(.cancel)
+//
+//            let alertController = UIAlertController(title: "Action not allowed", message: "Tapping on links is not allowed. Sorry!", preferredStyle: .alert)
+//            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//            present(alertController, animated: true, completion: nil)
+//            return
+//        }
+//        decisionHandler(.allow)
+//    }
+//}
