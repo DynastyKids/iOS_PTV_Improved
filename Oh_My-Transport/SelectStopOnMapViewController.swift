9408b3f2ae475df895625eba1aa5332b78b9d03c
DanistyWuKids(づ｡◕‿‿◕｡)づ <wgong0001@gmail.com>
2019-05-12 03:01:45 +1000

    Inital data struct build for Mapview

--- Oh_My-Transport/SelectStopOnMapViewController.swift
+++ Oh_My-Transport/SelectStopOnMapViewController.swift
@@ -10,7 +10,53 @@ import UIKit
 import MapKit
 import CoreLocation
 
+struct findStopByGPSRoot: Codable {
+    var stops: StopGeosearch?
+//    var disruptions: disruptions?
+    var status: findStopByGPSstatus?
+    
+    private enum CodingKeys: String, CodingKey{
+        case stops
+        case status
+    }
+}
+
+struct StopGeosearch: Codable{
+    var stopDistance: Double?
+    var stopSuburb: String?
+    var stopName: String?
+    var stopId: Int?
+    var routeType: Int?
+    var stopLatitude: Double?
+    var stopLongitude: Double?
+    var stopSequence: Int?
+    
+    private enum CodingKeys: String, CodingKey{
+        case stopDistance = "stop_distance"
+        case stopSuburb = "stop_suburb"
+        case stopName = "stop_name"
+        case stopId = "stop_id"
+        case routeType = "route_type"
+        case stopLatitude = "stop_latitude"
+        case stopLongitude = "stop_longitude"
+        case stopSequence = "stop_sequence"
+    }
+}
+
+struct findStopByGPSstatus: Codable {
+    var version: String?
+    var health: Int?
+    private enum CodingKeys: String, CodingKey{
+        case version
+        case health
+    }
+}
+
 class SelectStopOnMapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    let hardcodedURL:String = "https://timetableapi.ptv.vic.gov.au"
+    let hardcodedDevID:String = "3001122"
+    let hardcodedDevKey:String = "3c74a383-c69a-4e8d-b2f8-2e4c598b50b2"
 
     var mainMapView: MKMapView!
     var locManager = CLLocationManager()
@@ -23,11 +69,8 @@ class SelectStopOnMapViewController: UIViewController, CLLocationManagerDelegate
         super.viewDidLoad()
         locManager.requestWhenInUseAuthorization()
 
-        //使用代码创建
         self.mainMapView = MKMapView(frame:self.view.frame)
         self.view.addSubview(self.mainMapView)
-        
-        //地图类型设置 - 标准地图
         self.mainMapView.mapType = MKMapType.standard
         
         //创建一个MKCoordinateSpan对象，设置地图的范围（越小越精确）
@@ -67,9 +110,7 @@ class SelectStopOnMapViewController: UIViewController, CLLocationManagerDelegate
             self.locationManager.startUpdatingLocation()
         }
         
-        
-        
-        //大头针对象
+        //大头针对象 - 添加车站用
         let claytonStation = MKPointAnnotation()    //创建一个大头针对象
         claytonStation.coordinate = CLLocation(latitude: -37.9251671,longitude: 145.120682).coordinate  //设置大头针的显示位置
         claytonStation.title = "Clayton Station"    //设置点击大头针之后显示的标题
@@ -111,5 +152,21 @@ class SelectStopOnMapViewController: UIViewController, CLLocationManagerDelegate
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    fileprivate func extractedFunc(_ request: String) -> String {
+        let signature: String = request.hmac(algorithm: CryptoAlgorithm.SHA1, key: hardcodedDevKey)
+        let requestAddress: String = hardcodedURL+request+"&signature="+signature
+        
+        return requestAddress
+    }
+    func findStopByMap(latitude: Double, longitude: Double) -> String {
+        let request: String = "/v3/stops/location/"+String(latitude)+","+String(longitude)+"?max_results=100&devid="+hardcodedDevID
+        return extractedFunc(request)
+    }
+    
+    func findStopWithZoom(latitude: Double, longitude: Double, stopQuantity: Int, distance: Double) -> String{
+        let request: String = "/v3/stops/location/"+String(latitude)+","+String(longitude)+"?max_results="+String(stopQuantity)+"&max_distance"+String(distance)+"&devid="+hardcodedDevID
+        return extractedFunc(request)
+    }
 }
